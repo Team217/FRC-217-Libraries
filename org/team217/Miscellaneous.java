5e82583ee16cce5bbb6c115aebb595a46ce69ec6
Benjamin Hall <hallb1016@gmail.com>
2019-01-21 17:06:06 -0500

    Version 1.2.0
    Added Miscellaneous.withinRange()
    - Checks if a given value is within a range

--- org/team217/Miscellaneous.java
+++ org/team217/Miscellaneous.java
@@ -8,14 +8,14 @@ package org.team217;
 public class Miscellaneous {
     
     /**
-	 * Checks if the joystick is in a given deadband and, if it is, sets the joystick value to 0.
+	 * Checks if the value is in a given deadband and, if it is, sets the value to 0.
 	 * 
 	 * @param value
-	 *        The joystick value
+	 *        The tested value
 	 * @param deadband
 	 *        The deadband size
 	 * @return
-	 *        The joystick value if not in the deadband, 0.0 if in the deadband
+	 *        The value if not in the deadband, 0.0 if in the deadband
 	 * 
 	 * @author ThunderChickens 217
 	 */
@@ -25,5 +25,24 @@ public class Miscellaneous {
 		}
 		
 		return value;
+	}
+
+	/**
+	 * Checks if the value is within a given range
+	 * 
+	 * @param value
+	 *           The tested value
+	 * @param lower
+	 *           The lower range
+	 * @param upper
+	 *           The upper range
+	 * @return
+	 *           {@code true} if the value is within the range
+	 */
+	public static boolean withinRange(double value, double lower, double upper) {
+		if (value >= lower && value <= upper) {
+			return true;
+		}
+		return false;
 	}
 }
